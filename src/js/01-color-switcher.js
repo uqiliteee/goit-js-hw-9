7078ade159b144c8cd6129871902791fe85f35b9
Karina <meriucakarina@gmail.com>
2022-09-29 16:27:02 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+
+let timerId = null;
+
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+
+function onStartBtn() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        startBtn.setAttribute('disabled', 'disabled');
+    }, 1000);
+
+    
+}
+
+startBtn.removeAttribute('disabled', '')
+
+function onStopBtn() {
+    clearInterval(timerId);
+    stopBtn.removeAttribute('disabled', 'disabled')
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
