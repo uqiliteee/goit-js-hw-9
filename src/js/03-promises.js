c158544aa3e89c0a2ae5818cce4e3112a2c77073
Karina <meriucakarina@gmail.com>
2022-09-30 21:31:53 +0300

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+console.log(form);
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let delayV = Number(delay.value);
+  let stepV = Number(step.value);
+  let amountV = Number(amount.value);
+
+  for (let i = 0; i < amountV; i++) {
+    createPromise(i, delayV)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayV -= stepV;
+
   }
-}
+
+  event.currentTarget.reset();
+};
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      };
+    }, delay);
+    
+  });
+};
+ 
+
+
+
