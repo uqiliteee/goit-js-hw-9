0300c2edf0a3e12001960d9cbaa606022b06772f
Karina <meriucakarina@gmail.com>
2022-10-01 12:34:57 +0300

    bug fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,25 +13,27 @@ function onFormSubmit(event) {
     elements: { delay, step, amount },
   } = event.currentTarget;
 
-  let delayV = Number(delay.value);
+  let delayV= Number(delay.value);
   let stepV = Number(step.value);
   let amountV = Number(amount.value);
 
   for (let i = 0; i < amountV; i++) {
-    createPromise(i, delayV)
+    let position = i + 1;
+    console.log(position)
+    let delayTgt = delayV + i * stepV
+    console.log(delayTgt)
+    createPromise(position, delayTgt)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-    delayV -= stepV;
+      }); 
 
   }
 
   event.currentTarget.reset();
 };
-
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -45,7 +47,7 @@ function createPromise(position, delay) {
     
   });
 };
- 
+console.log(createPromise(1000, 1500));
 
 
 
